d497626ce2951e87a3c9b9c354c75b1d0e79ad46
saikatp29 <saikat.pattanaik@kaleidofin.com>
2025-05-30 19:21:31 +0530

    Product Service initial code added

---
+++ backend/product-service/src/main/java/com/product/product/model/Product.java
@@ -0,0 +1,35 @@
+package com.product.product.model;
+
+import com.product.product.enums.ProductCategory;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "expiry_date")
+    private LocalDate expiryDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "product_image_url")
+    private String productImage;
+}
